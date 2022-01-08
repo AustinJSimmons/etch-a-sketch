c362692ee8e3d55de5fd987ab632bdf1da765098
austin <austinsimmons098@gmail.com>
2022-01-07 22:24:11 -0700

    Added RGB button and black button, both function

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 function calcDivSize(num) {
-    let pixels = 600/num - 2;
+    let pixels = 600/num;
     return pixels;
 }
 
 function clear() {
-     const divs = document.querySelectorAll('.colored');
+     const divs = document.querySelectorAll('.ink');
 
      divs.forEach((div) => {
-        div.classList.remove('colored');
+        div.style.backgroundColor = 'whitesmoke';
      });
+}
+
+function resize() {
+    const divs = document.querySelectorAll('.ink');
+
+    divs.forEach((div) => {
+        gridContainer.removeChild(div)
+    });
+
+    num = Number(prompt('Enter a number < 100'));
+    pixels = calcDivSize(num);
+    for (let numOfRows = 1; numOfRows <= num; numOfRows++) {
+        for (let numberOfDivs = 1; numberOfDivs <= num; numberOfDivs++) {
+            let div = document.createElement('div');
+            div.classList.add('ink');
+            div.style.width = `${pixels}px`;
+            div.style.height = `${pixels}px`;
+            gridContainer.appendChild(div);
+            
+        }
+    }
+    addMousover();
+    shake();
 }
 
 function shake() {
@@ -16,15 +39,62 @@ function shake() {
     button.addEventListener('click', clear);
 }
 
+function resizeButton() {
+    const button = document.querySelector('.resize');
+    button.addEventListener('click', resize);
+}
+
+function rgbButton() {
+    const button = document.querySelector('.rainbow');
+    button.addEventListener('click', applyRGB);
+}
+
+function blackButton() {
+    const button = document.querySelector('.black');
+    button.addEventListener('click', addMousover);
+}
+
 function addMousover() {
     const divs = document.querySelectorAll('.ink');
 
     divs.forEach((div) => {
         div.addEventListener('mouseover', () => {
-            div.classList.add('colored')
+            div.style.backgroundColor = 'black';
         });
     });
 }
+
+function rgb() {
+    let x = randomNum();
+    let y = randomNum();
+    let z = randomNum();
+    const rgbArray = [];
+    rgbArray[0] = x;
+    rgbArray[1] = y;
+    rgbArray[2] = z;
+    let color = 'rgb(' + rgbArray.toString() + ')';
+    return color;
+}
+
+function randomNum() {
+    let x = Math.floor(Math.random() * 255);
+    return x
+}
+
+function applyRGB(color) {
+    const divs = document.querySelectorAll('.ink');
+
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = rgb();
+        })
+    });
+}
+
+
+
+
+
 
 let num = Number(prompt('Enter a number < 100'));
 let pixels = calcDivSize(num);
@@ -43,5 +113,8 @@ for (let numOfRows = 1; numOfRows <= num; numOfRows++) {
 
 addMousover();
 shake();
+resizeButton();
+rgbButton();
+blackButton();
 
 console.log(document.querySelectorAll('.ink'))
